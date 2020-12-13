d531d6e62e26c7aaa5a2aa832d03958eb9aed52b
Denis Kazakov <kazakovdeyu@yandex.ru>
2020-12-13 23:57:01 +0300

    get_next_date_time

--- booking_service/booking/tests/common.py
+++ booking_service/booking/tests/common.py
@@ -1,4 +1,4 @@
-from datetime import date as _date
+from datetime import date as _date, timedelta as _td
 from random import sample as _sample
 from string import ascii_letters as _letters
 
@@ -12,7 +12,9 @@ CREDENTIALS = {'username': USERNAME, 'password': PASSWORD}
 SPECIALTY = 'therapist'
 
 TODAY = _date.today()
-START_TIME = 9
+START_TIME, END_TIME = 9, 17
+used_date = TODAY
+used_time = START_TIME
 
 
 class URL:
@@ -30,4 +32,25 @@ class URL:
 
 
 def random_str(length: int = 10) -> str:
+    """Получить случайную строку указанной длины"""
     return ''.join(_sample(_letters, length))
+
+
+def get_next_date_time(fmt: str = ''):
+    """Получить следующие валидные дату и время"""
+    global used_date, used_time
+
+    if used_date.isoweekday() > 5:
+        used_date += _td(days=2)
+
+    date, time = used_date, used_time
+    if used_time == END_TIME:
+        used_date += _td(days=1)
+        used_time = START_TIME
+    else:
+        used_time += 1
+
+    if fmt:
+        date = date.strftime(fmt)
+
+    return date, time
